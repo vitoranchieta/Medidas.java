cd1324ba97d8ed31f10203d3d5d4d95a5ead72cb
“Vitor” <vitorfagundes.7@gmail.com>
2021-06-05 22:06:31 -0300

    Inclusão do "Bom dia" no começo da impressão de texto no Console.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -12,9 +12,9 @@ public class Medidas {
     // 3.1 - Atributos - Características
 
 
-
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
+        System.out.println("Bom dia!");
         System.out.println("Cálculo de areas");
 
         //Calculo de área - Reduzido
